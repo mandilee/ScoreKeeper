ac4e385a714546cb56cb2a240452bb0a77c53104
Mandi Lee <github@mandilee.co.uk>
2017-04-27 17:56:39 +0100

    Minor fixes

--- app/src/main/java/uk/co/mandilee/scorekeeper/MainActivity.java
+++ app/src/main/java/uk/co/mandilee/scorekeeper/MainActivity.java
@@ -9,24 +9,20 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private EditText teamANameText;
     private EditText teamBNameText;
-    private EditText minutesText;
     private EditText pointsText;
     private EditText roundsText;
 
     // grab the default values for the fields
     private String teamAName;
     private String teamBName;
-    private float minutes;
     private float points;
     private int rounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         // grab the button
@@ -36,28 +32,24 @@ public class MainActivity extends AppCompatActivity {
         // get the EditText fields for later
         teamANameText = (EditText) findViewById(R.id.teamAName);
         teamBNameText = (EditText) findViewById(R.id.teamBName);
-        minutesText = (EditText) findViewById(R.id.minutes);
         pointsText = (EditText) findViewById(R.id.points);
         roundsText = (EditText) findViewById(R.id.rounds);
 
         beginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Boolean nextPage;
-
+                Boolean nextPage = true;
                 teamAName = teamANameText.getText().toString();
                 teamBName = teamBNameText.getText().toString();
 
-                if (nextPage = isFloat(minutesText.getText().toString())) {
-                    minutes = Float.valueOf(minutesText.getText().toString());
-                } else {
-                    nextPage = false;
-                }
+                // check that points entered are numeric
                 if (isFloat(pointsText.getText().toString())) {
                     points = Float.valueOf(pointsText.getText().toString());
                 } else {
                     nextPage = false;
                 }
+
+                // check that rounds entered are numeric
                 if (isInt(roundsText.getText().toString())) {
                     rounds = Integer.valueOf(roundsText.getText().toString());
                 } else {
@@ -68,35 +60,36 @@ public class MainActivity extends AppCompatActivity {
                     Intent scoreKeeperIntent = new Intent(MainActivity.this, ScoreKeeper.class);
                     scoreKeeperIntent.putExtra("TEAM_A_NAME", teamAName);
                     scoreKeeperIntent.putExtra("TEAM_B_NAME", teamBName);
-                    scoreKeeperIntent.putExtra("MINUTES", minutes);
                     scoreKeeperIntent.putExtra("POINTS", points);
                     scoreKeeperIntent.putExtra("ROUNDS", rounds);
                     startActivity(scoreKeeperIntent);
                 } else {
-                    Toast.makeText(getApplicationContext(), "Minutes, Points and Rounds must be numbers", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Points and Rounds must be numbers", Toast.LENGTH_LONG).show();
                 }
             }
         });
-
 
         defaultBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 teamAName = getString(R.string.default_team_a);
                 teamBName = getString(R.string.default_team_b);
-                minutes = Float.valueOf(getString(R.string.default_minutes));
                 points = Float.valueOf(getString(R.string.default_points));
                 rounds = Integer.valueOf(getString(R.string.default_rounds));
-
                 teamANameText.setText(teamAName);
                 teamBNameText.setText(teamAName);
-                minutesText.setText(String.valueOf(minutes));
                 pointsText.setText(String.valueOf(points));
                 roundsText.setText(String.valueOf(rounds));
             }
         });
     }
 
+    /**
+     * check if given parameter is a float
+     *
+     * @param value - see if this is a float
+     * @return true if no exception thrown; false if exception thrown (not a float)
+     */
     private boolean isFloat(String value) {
         try {
             Float.parseFloat(value); // intentionally ignored result
@@ -106,6 +99,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * check if given parameter is an integer
+     * @param value - see if this is an integer
+     * @return true if no exception thrown; false if exception thrown (not an integer)
+     */
     private boolean isInt(String value) {
         try {
             Integer.parseInt(value); // intentionally ignored result
--- app/src/main/java/uk/co/mandilee/scorekeeper/ScoreKeeper.java
+++ app/src/main/java/uk/co/mandilee/scorekeeper/ScoreKeeper.java
@@ -6,7 +6,16 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * Future Improvements:
+ * 1. Change onclick to onClickListeners
+ * 2. Pull Teams into a new Class
+ * 3. Add a timer  & subsequently 'sudden death' mode
+ * 4. Learn how to implement comments properly and use them!!
+ */
+
 public class ScoreKeeper extends AppCompatActivity {
+
     private TextView akaScoreTextView;
     private TextView akaWinsTextView;
     private TextView shiroScoreTextView;
@@ -14,12 +23,13 @@ public class ScoreKeeper extends AppCompatActivity {
 
     private float akaScore;
     private float shiroScore;
+
     private int akaWins;
     private int shiroWins;
 
     private String teamAName;
     private String teamBName;
-    private float minutes;
+
     private float points;
     private int rounds;
 
@@ -40,12 +50,12 @@ public class ScoreKeeper extends AppCompatActivity {
         TextView teamA = (TextView) findViewById(R.id.teamAName);
         teamAName = getIntent().getStringExtra("TEAM_A_NAME");
         teamA.setText(teamAName);
+
         TextView teamB = (TextView) findViewById(R.id.teamBName);
         teamBName = getIntent().getStringExtra("TEAM_B_NAME");
         teamB.setText(teamBName);
 
         // grab settings from previous activity
-        minutes = getIntent().getFloatExtra("MINUTES", R.string.default_minutes);
         points = getIntent().getFloatExtra("POINTS", R.string.default_points);
         rounds = getIntent().getIntExtra("ROUNDS", R.string.default_rounds);
 
@@ -59,92 +69,83 @@ public class ScoreKeeper extends AppCompatActivity {
             resetScores();
             resetWins();
         }
-    }// This callback is called only when there is a saved instance previously saved using
+    }
 
-    // onSaveInstanceState(). We restore some state in onCreate() while we can optionally restore
-    // other state here, possibly usable after onStart() has completed.
-    // The savedInstanceState Bundle is same as the one used in onCreate().
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
-        //*
         akaScoreTextView.setText(String.valueOf(savedInstanceState.getFloat("AKA_SCORE")));
         shiroScoreTextView.setText(String.valueOf(savedInstanceState.getFloat("SHIRO_SCORE")));
         akaWinsTextView.setText(String.valueOf(savedInstanceState.getInt("AKA_WINS")));
         shiroWinsTextView.setText(String.valueOf(savedInstanceState.getInt("SHIRO_WINS")));
-        //*/
     }
 
-    // invoked when the activity may be temporarily destroyed, save the instance state here
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putFloat("AKA_SCORE", akaScore);
         outState.putFloat("SHIRO_SCORE", shiroScore);
         outState.putInt("AKA_WINS", akaWins);
         outState.putInt("SHIRO_WINS", shiroWins);
-
-        // call superclass to save any view hierarchy
         super.onSaveInstanceState(outState);
     }
 
-    /*
-        Add one point to the Aka Score
+    /**
+     * Add one point to the Aka Score
      */
     public void akaIppon(View v) {
         akaScore += 1;
         didAkaWin();
     }
 
-
-    /*
-        Add half a point to the Aka Score
+    /**
+     * Add half a point to the Aka Score
      */
     public void akaWazaAri(View v) {
         akaScore += 0.5;
         didAkaWin();
     }
 
-    /*
-        Aka First Warning - Half a penalty point to Shiro
+    /**
+     * Aka First Warning - Half a penalty point to Shiro
      */
     public void akaKeikoku(View v) {
         shiroScore += 0.5;
         didShiroWin();
     }
 
-    /*
-        Aka Second Warning - One penalty point to Shiro
+    /**
+     * Aka Second Warning - One penalty point to Shiro
      */
     public void akaHansokuShui(View v) {
         shiroScore += 1;
         didShiroWin();
     }
 
-    /*
-        Aka Won
+    /**
+     * Aka Won
      */
     public void akaNoKachi(View v) {
         akaScore = points;
         didAkaWin();
     }
 
-    /*
-        Set score in aka_score TextView
+    /**
+     * Set score in aka_score TextView
      */
     private void akaSetScore() {
         akaScoreTextView.setText(String.valueOf(akaScore));
     }
 
-    /*
-        Set number of wins in aka_wins TextView
+    /**
+     * Set number of wins in aka_wins TextView
      */
     private void akaSetWins() {
         akaWinsTextView.setText(String.valueOf(akaWins));
     }
 
-    /*
-        Check to see if Aka won (3 points = win)
-        If they did, add 1 win and reset both scores.
-        Otherwise, set Aka score
+    /**
+     * Check to see if Aka won
+     * If they did, add 1 win and reset both scores.
+     * Otherwise, set Aka score
      */
     private void didAkaWin() {
         if (akaScore >= points) {
@@ -161,70 +162,68 @@ public class ScoreKeeper extends AppCompatActivity {
         }
     }
 
-
-    /*
-        Add one point to the Shiro Score
+    /**
+     * Add one point to the Shiro Score
      */
     public void shiroIppon(View v) {
         shiroScore += 1;
         didShiroWin();
     }
 
-    /*
-        Add half a point to the Shiro Score
+    /**
+     * Add half a point to the Shiro Score
      */
-    public void shiroWazaAri(View v) { // half a point to shiro/Red Team
+    public void shiroWazaAri(View v) {
         shiroScore += 0.5;
         didShiroWin();
     }
 
-    /*
-        Shiro First Warning - Half a penalty point to Aka
+    /**
+     * Shiro First Warning - Half a penalty point to Aka
      */
     public void shiroKeikoku(View v) {
         akaScore += 0.5;
         didAkaWin();
     }
 
-    /*
-        Shiro Second Warning - One penalty point to Aka
+    /**
+     * Shiro Second Warning - One penalty point to Aka
      */
-    public void shiroHansokuShui(View v) { // shiro Warning; one penalty point to Shiro/White Team
+    public void shiroHansokuShui(View v) {
         akaScore += 1;
         didAkaWin();
     }
 
-    /*
-        Shiro Won
+    /**
+     * Shiro Won
      */
     public void shiroNoKachi(View v) {
         shiroScore = points;
         didShiroWin();
     }
 
-    /*
-        Set score in shiro_score TextView
+    /**
+     * Set score in shiro_score TextView
      */
     private void shiroSetScore() {
         shiroScoreTextView.setText(String.valueOf(shiroScore));
     }
 
-    /*
-        Set number of wins in shiro_wins TextView
+    /**
+     * Set number of wins in shiro_wins TextView
      */
     private void shiroSetWins() {
         shiroWinsTextView.setText(String.valueOf(shiroWins));
     }
 
-    /*
-        Check to see if Shiro won (3 points = win)
-        If they did, add 1 win and reset both scores.
-        Otherwise, set Shiro score
+    /**
+     * Check to see if Shiro won
+     * If they did, add 1 win and reset both scores.
+     * Otherwise, set Shiro score
      */
     private void didShiroWin() {
         if (shiroScore >= points) {
             shiroWins += 1;
-
             if (akaWins >= rounds) {
                 Toast.makeText(getApplicationContext(), teamBName + " WINS!", Toast.LENGTH_LONG).show();
             } else {
@@ -237,14 +236,13 @@ public class ScoreKeeper extends AppCompatActivity {
         }
     }
 
-    /*
-        Reset all scores and wins
+    /**
+     * Reset all scores and wins
      */
     public void reset(View v) {
         resetScores();
         resetWins();
     }
-
 
     private void resetScores() {
         akaScore = 0;
